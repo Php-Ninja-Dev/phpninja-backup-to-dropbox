5e6253ed01a847358b4be2e07f661c612db45002
Beto <betolopezayesa@gmail.com>
2023-07-23 09:21:40 +0200

    Update backup-to-dropbox.sh
    + Chunks of 100MB to bypass Dropbox limit
    + SQL is zipped too

--- backup-to-dropbox.sh
+++ backup-to-dropbox.sh
@@ -18,34 +18,63 @@ dropbox_token="YOUR DROPBOX TOKEN"
 hostname="THIS IS THE NAME OF THE FUTURE FOLDER IN DROPBOX"
 
 # Backup Folder and Zip File
-backup_date=$(date +%Y-%m-%d_%H:%M:%S)
-target_folder="www"
-zip_file="backup_${hostname}_${backup_date}.zip"
+backup_date=$(date +%Y-%m-%d)
+www_folder="www"
+zip_file="files_${hostname}_${backup_date}.zip"
 sql_file="database_${hostname}_${backup_date}.sql"
+sql_zip_file="database_${hostname}_${backup_date}.zip"
 
-# Backup MySQL Database
-mysqldump -h "${db_host}" -P "${db_port}" -u "${db_user}" -p"${db_password}" "${db_name}" --no-tablespaces > "${sql_file}"
-
-# Zip the www/ folder
-zip -r "${zip_file}" "${target_folder}" -x "cache/*" "tmp/*" ".git/*" "git/*" "ninja/*" 
-
-# Dropbox Upload Function
+# Upload both files to Dropbox
 upload_to_dropbox() {
     local source_file="$1"
     local dropbox_path="/${hostname}/${backup_date}/${source_file}"
     local dropbox_api_url="https://content.dropboxapi.com/2/files/upload"
-	  echo "${dropbox_path}"
+    echo "${dropbox_path}"
     curl -X POST "${dropbox_api_url}" \
-         --header "Authorization: Bearer ${dropbox_token}" \
+  	 --header "Authorization: Bearer ${dropbox_token}" \
          --header "Content-Type: application/octet-stream" \
          --header "Dropbox-API-Arg: {\"path\":\"${dropbox_path}\", \"mode\":\"add\"}" \
-         --data-binary @"${source_file}"
+         --data-binary @"${source_file}"         
 }
 
-# Upload both files to Dropbox
-upload_to_dropbox "${sql_file}"
-upload_to_dropbox "${zip_file}"
+echo "========================"
+echo "BACKUP OF ${hostname}"
+echo "Date: ${backup_date} - Folder: ${www_folder}"
+echo "Base files: ${zip_file} & ${sql_file}"
+echo "========================"
 
-# Clean up temporary files
+# Backup MySQL Database
+echo "Starting DATABASE BACKUP...."
+mysqldump -h "${db_host}" -P "${db_port}" -u "${db_user}" -p"${db_password}" "${db_name}" --no-tablespaces > "${sql_file}"
+
+echo "Zipping SQL file..."
+zip -s 100M "${sql_zip_file}" "${sql_file}"
+echo "Cleaning SQL file ..."
 rm "${sql_file}"
-rm "${zip_file}"
+
+echo "Uploading DATABASE to DROPBOX..."
+
+for zip_chunk in "database_${hostname}_${backup_date}"*; do
+	echo "Uploading a database zip chunk to dropbox"
+    	upload_to_dropbox "${zip_chunk}"
+   	rm "${zip_chunk}"
+done
+
+
+# ZIP
+echo "========================"
+echo "Files Backup Zipping ... "
+zip -r -s 100M "${zip_file}" "${www_folder}" -x "cache/*" "tmp/*" ".git/*" "git/*" "ninja/*"
+
+# Upload zip chunks
+echo "Uploading ZIP chunks to DROPBOX..."
+for zip_chunk in "files_${hostname}_${backup_date}"*; do
+	echo "Uploading zip chunk to Dropbox ..."
+    	upload_to_dropbox "${zip_chunk}"
+    	# Clean up temporary files
+	echo "Cleaning ... "
+   	rm "${zip_chunk}"
+done
+
+
+echo "Completed ------"
